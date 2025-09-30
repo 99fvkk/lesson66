8d2a073bc2913c6552163d4c5937c02ae0e1a163
Aleshka1111 <akeksejpromyslov4@gmail.com>
2025-09-30 21:00:36 -0200

    Исправил функцию удадения, заполнил таблицы

--- project6.py
+++ project6.py
@@ -1,5 +1,5 @@
-import sqlite3
-db_connect = sqlite3.connect('project666.db')
+from repo import*
+db_connect = connect('project666.db')
 db_cursor = db_connect.cursor() 
 #h
 def create_table():
@@ -14,9 +14,8 @@ def create_table():
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       title TEXT NOT NULL,
                       author TEXT NOT NULL,
-                      genre INTEGER NOT NULL CHECK(total >=1),
-                      free INTEGER NOT NULL CHECK(free >= 0 and free <=total)
-                      )""")
+                      genre INTEGER NOT NULL,
+                      free INTEGER NOT NULL )""")
     db_cursor.execute("""CREATE TABLE IF NOT EXISTS loans 
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       pr TEXT NOT NULL REFERENCES readers(pr),
@@ -28,6 +27,14 @@ def create_table():
                       book_id INTEGER NOT NULL REFERENCES books(id),
                       date TEXT NOT NULL 
                       )""")
+   
+    print(db_cursor.fetchall())
+    
     db_connect.commit()
 create_table()
+
+delete_book(db_connect, "book1", "author1")
+
+db_cursor.execute('''INSERT INTO books VALUES (1, 'book1', 'author1', 1, 0)''')
+db_cursor.execute('''SELECT * FROM books''')
 db_connect.close()
--- repo.py
+++ repo.py
@@ -1,11 +1,12 @@
 from sqlite3 import*
-conn=connect('project666.db') 
-cursor=conn.cursor()
-def delete_book():
-    id_1=int(input('введите id '))
-    cursor.execute("SELECT books.id FROM books JOIN loans book_id.id=books.id")
-    loans_id=cursor.fetchall()
-    if id_1 in loans_id:
-        print("Вы не можете удалить эту книгу")
-    else:
-        cursor.execute("DELETE FROM books WHERE id = id_1")
+
+def delete_book(conn, title, author):
+    cursor=conn.cursor()
+    cursor.execute('''SELECT id FROM books WHERE title=? AND author=? AND free = 0''', 
+                   (title, author))
+    id_1=cursor.fetchall()
+
+    cursor.execute(f'''DELETE FROM books WHERE id = {id_1[0][0]}''')
+    cursor.execute('''SELECT * FROM books''')
+    print(cursor.fetchall())
+
