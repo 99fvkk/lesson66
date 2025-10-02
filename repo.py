31106492ba8455fac1647ffcf74cb58f08c2f0f3
Aleshka1111 <akeksejpromyslov4@gmail.com>
2025-10-02 19:10:54 -0200

    commit2

--- repo.py
+++ repo.py
@@ -5,7 +5,6 @@ def delete_book(conn, title, author):
     cursor.execute('''SELECT id FROM books WHERE title=? AND author=? AND free = 0''', 
                    (title, author))
     id_1=cursor.fetchall()
-
     cursor.execute(f'''DELETE FROM books WHERE id = {id_1[0][0]}''')
     cursor.execute('''SELECT * FROM books''')
     print(cursor.fetchall())
