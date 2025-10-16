f7ef26c11f1e3f0f60c38f48ae66bfaccfb03aa8
99fvkk <tymb04kaaa@mail.ru>
2025-10-16 23:42:58 +0300

    Update project6.py
    поправил таблицу

--- project6.py
+++ project6.py
@@ -1,4 +1,4 @@
-from repo import*
+from sqlite3 import*
 db_connect = connect('project666.db')
 db_cursor = db_connect.cursor() 
 #h
@@ -13,20 +13,20 @@ def create_table():
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       title TEXT NOT NULL,
                       author TEXT NOT NULL,
-                      genre INTEGER NOT NULL,
-                      total INTEGER NOT NULL,
-                      free INTEGER NOT NULL )""")
+                      genre TEXT NOT NULL,
+                      total INTEGER NOT NULL CHECK(total >= 1),
+                      free INTEGER NOT NULL CHECK(free >= 0 and free <=total) )""")
     db_cursor.execute("""CREATE TABLE IF NOT EXISTS loans 
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       pr TEXT NOT NULL REFERENCES readers(pr),
                       book_id INTEGER NOT NULL REFERENCES books(id),
-                      date TEXT NOT NULL 
+                      date TEXT NOT NULL CHECK(date GLOB '[0-3][0-9]/[0-1][0-9]/[0-9][0-9]')
                      )""")
     db_cursor.execute("""CREATE TABLE IF NOT EXISTS holds
                       (id INTEGER PRIMARY KEY AUTOINCREMENT,
                       pr TEXT NOT NULL REFERENCES readers(pr),
                       book_id INTEGER NOT NULL REFERENCES books(id),
-                      date TEXT NOT NULL 
+                      date TEXT NOT NULL CHECK(date GLOB '[0-3][0-9]/[0-1][0-9]/[0-9][0-9]')
                       )""")
     db_cursor.execute('''SELECT * FROM books''')
     print(db_cursor.fetchall())
@@ -36,4 +36,4 @@ create_table()
 db_cursor.execute('''SELECT * FROM books''')
 print(db_cursor.fetchall())
 
-db_connect.close()
+#db_connect.close()
